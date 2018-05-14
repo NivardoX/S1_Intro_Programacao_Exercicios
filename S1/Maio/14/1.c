a4b28e8bba6fc26bd1674340c82a0fda004bfa69
Nivardo Albuquerque <nivardoti@outlook.com>
2018-05-14 17:50:52 -0300

    Create 1.c

---
+++ S1/Maio/14/1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mdcRecursao(long long int *a,long long int *b){
+	if(*a == *b){
+		return *a;
+	}else if(*a > *b){
+		*a = *a - *b;
+		mdcRecursao(a,b);		
+	}else{
+		*b = *b -*a;	
+		mdcRecursao(a,b);	
+	}
+}
+
+int main(int argc, char **argv){
+	long long int i,j;
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	
+	printf("%d\n",mdcRecursao(&i,&j));
+	
+	return 0;
+}
+	
+	
