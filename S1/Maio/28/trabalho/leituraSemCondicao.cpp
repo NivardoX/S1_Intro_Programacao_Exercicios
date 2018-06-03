365ad5fe87b6ce301d8542a958c5ea4f51cac11b
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-06-03 23:16:14 +0000

    Create leituraSemCondicao.cpp

---
+++ S1/Maio/28/trabalho/leituraSemCondicao.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+	int i,j;
+	char lixo[1000];
+	unsigned char temp;
+
+	cout << "lendo imagem\n";
+
+	for(i = 1; i < x; i++){
+		for(j = 1; j < y; j++){
+			
+			fscanf(pgm, "%hhu ", &temp);
+			M[i][j] = temp;
+
+		}
+	}
+
+}
